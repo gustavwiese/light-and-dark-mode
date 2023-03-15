befb4bcd0371f58fc92159fed37122a7b99072df
gustavwiese <gusse12345@gmail.com>
2023-03-15 11:41:45 +0100

    1

--- app.js
+++ app.js
@@ -3,11 +3,31 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    //event listener for select on HTML
-    document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
+  //event listener for select on HTML
+  document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
-    console.log("New color mode selected");
+  console.log("New color mode selected");
+  const selectedColorMode = this.value;
+  console.log(selectedColorMode);
+  changeMode(selectedColorMode);
+}
+
+function changeMode(mode) {
+  resetColorMode();
+  if (mode == "dark") {
+    document.body.classList.add("dark-mode");
+  } else if (mode === "pink") {
+    document.body.classList.add("pink-mode");
+  } else if (mode === "green") {
+    document.body.classList.add("green-mode");
+  } else if (mode === "blue") {
+    document.body.classList.add("blue-mode");
+  }
+}
+
+function resetColorMode() {
+  document.body.className = "";
 }
